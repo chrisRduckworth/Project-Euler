be66bcef1f844d452225d1f1ab1b7184f3203f58
chrisrduckworth <c_duckworth@hotmail.com>
2024-01-25 18:03:21 +0000

    add test for smallest multiple

--- 005-smallest_multiple/test/smallest_multiple_test.cpp
+++ 005-smallest_multiple/test/smallest_multiple_test.cpp
@@ -1,2 +1,9 @@
 #include <gtest/gtest.h>
 #include "../smallest_multiple.hpp"
+
+TEST(SmallestMultipleTest, ReturnsSmallestMultiple) {
+  ASSERT_EQ(SmallestMultiple(3), 2);
+  ASSERT_EQ(SmallestMultiple(6), 60);
+  ASSERT_EQ(SmallestMultiple(11), 2520);
+  
+}
