0bc4c602de49bb55e3ae5bad8db60d14278c7c34
chrisrduckworth <c_duckworth@hotmail.com>
2024-01-26 16:50:29 +0000

    add smallest multiple function

--- 005-smallest_multiple/smallest_multiple.cpp
+++ 005-smallest_multiple/smallest_multiple.cpp
@@ -32,3 +32,38 @@ std::unordered_map<int, int> PrimeDecomposition(int n) {
   return prime_decomp;
 }
 
+int SmallestMultiple(int n) {
+  // returns the smallest number which is a multiple of
+  // all numbers in [1, n)
+
+  std::unordered_map<int, int> primes = {};
+
+  // the lcm of numbers is the max of all the
+  // exponents of the prime factors of those 
+  // numbers
+
+  for (int i = 2; i < n; i++) {
+
+    std::unordered_map<int, int> prime_decomp = PrimeDecomposition(i);
+
+    for (const auto& [prime, count] : prime_decomp) {
+
+      if ((primes.contains(prime) && count > primes[prime])
+        || 
+        !primes.contains(prime)) 
+      {
+        primes[prime] = count;
+      }
+
+    }
+
+  }
+
+  int total = 1;
+
+  for (const auto &[prime, count] : primes) {
+    total *= std::pow(prime, count);
+  }
+  
+  return total;
+}
