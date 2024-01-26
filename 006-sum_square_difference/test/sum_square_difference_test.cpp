25d60c978207451209a8a749034502cf8f331cb5
chrisrduckworth <c_duckworth@hotmail.com>
2024-01-26 17:11:04 +0000

    add test for sum square difference

--- 006-sum_square_difference/test/sum_square_difference_test.cpp
+++ 006-sum_square_difference/test/sum_square_difference_test.cpp
@@ -1,2 +1,8 @@
 #include <gtest/gtest.h>
 #include "../sum_square_difference.hpp"
+
+TEST (SumSquareDifferenceTest, ReturnsCorrect) {
+  EXPECT_EQ(SumSquareDifference(2), 4);
+  EXPECT_EQ(SumSquareDifference(5), 170);
+  EXPECT_EQ(SumSquareDifference(10), 2640);
+}
