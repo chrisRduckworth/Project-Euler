c03e0f1919f05cdd524b2ccc6de5a0cb865a9492
chrisrduckworth <c_duckworth@hotmail.com>
2024-01-26 17:11:20 +0000

    add sum square difference function

--- 006-sum_square_difference/sum_square_difference.cpp
+++ 006-sum_square_difference/sum_square_difference.cpp
@@ -1,1 +1,12 @@
 #include "sum_square_difference.hpp"
+
+int SumSquareDifference(int n) {
+
+  // sum of first n *2 = 1/6 k (k + 1) (2k + 1)
+  int sum_squares = n * (n + 1) * (2 * n + 1) / 6;  
+
+  // sum of first n numbers = 1/2 k (k + 1)
+  int sum_squared = (n * n * (n + 1) * (n + 1) / 4);
+
+  return sum_squared - sum_squares;
+}
