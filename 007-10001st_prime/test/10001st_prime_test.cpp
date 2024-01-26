a5c57616ab22b78e66d12d2c22ea97a288cf9563
chrisrduckworth <c_duckworth@hotmail.com>
2024-01-26 17:29:25 +0000

    add tests for isprime

--- 007-10001st_prime/test/10001st_prime_test.cpp
+++ 007-10001st_prime/test/10001st_prime_test.cpp
@@ -1,2 +1,17 @@
 #include <gtest/gtest.h>
 #include "../10001st_prime.hpp"
+
+TEST(IsPrimeTest, ReturnsFalseIfNotPrime) {
+  ASSERT_FALSE(IsPrime(4));
+  ASSERT_FALSE(IsPrime(6));
+  ASSERT_FALSE(IsPrime(9));
+  ASSERT_FALSE(IsPrime(33));
+}
+
+TEST(IsPrimeTest, ReturnsTrueIfPrime) {
+  ASSERT_TRUE(IsPrime(2));
+  ASSERT_TRUE(IsPrime(3));
+  ASSERT_TRUE(IsPrime(11));
+  ASSERT_TRUE(IsPrime(53));
+}
+
