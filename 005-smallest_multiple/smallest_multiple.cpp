788a09d6056e560368e83c859fedd4a4884373e4
chrisrduckworth <c_duckworth@hotmail.com>
2024-01-26 16:50:18 +0000

    add prime decomposition function

--- 005-smallest_multiple/smallest_multiple.cpp
+++ 005-smallest_multiple/smallest_multiple.cpp
@@ -1,1 +1,34 @@
 #include "smallest_multiple.hpp"
+#include <unordered_map>
+#include <cmath>
+
+std::unordered_map<int, int> PrimeDecomposition(int n) {
+  // returns the prime decomposition of a number
+
+  std::unordered_map<int, int> prime_decomp = {};
+
+  int j = 2;
+
+  while (j <= n) {
+
+    if (n % j == 0) {
+      // j is a factor of n
+
+      if (prime_decomp.contains(j)) prime_decomp[j] ++;
+
+      else prime_decomp[j] = 1;
+
+      n /= j;
+
+    } else {
+      //j is not a factor so try the next number
+
+      j++;
+
+    }
+
+  }
+
+  return prime_decomp;
+}
+
---
+++ 005-smallest_multiple/smallest_multiple.hpp
@@ -0,0 +1,4 @@
+#include <unordered_map>
+
+int SmallestMultiple(int n);
+extern std::unordered_map<int, int> PrimeDecomposition(int n);
