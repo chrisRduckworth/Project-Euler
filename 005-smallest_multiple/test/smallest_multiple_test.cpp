32b1b5a9b9754bbbb25c4e9e814d74bb447b0f83
chrisrduckworth <c_duckworth@hotmail.com>
2024-01-26 16:42:00 +0000

    add tests for prime decomposition function

--- 005-smallest_multiple/test/smallest_multiple_test.cpp
+++ 005-smallest_multiple/test/smallest_multiple_test.cpp
@@ -1,9 +1,20 @@
 #include <gtest/gtest.h>
 #include "../smallest_multiple.hpp"
+#include <unordered_map>
+
+TEST(PrimeDecompositionTest, ReturnsComposition) {
+  std::unordered_map<int, int> expected = {
+    {2, 2},
+    {3, 3},
+    {5, 1},
+    {7, 1}
+  };
+  std::unordered_map<int, int> decomp = PrimeDecomposition(3780);
+  EXPECT_EQ(decomp, expected);
+}
 
 TEST(SmallestMultipleTest, ReturnsSmallestMultiple) {
   ASSERT_EQ(SmallestMultiple(3), 2);
   ASSERT_EQ(SmallestMultiple(6), 60);
   ASSERT_EQ(SmallestMultiple(11), 2520);
-  
 }
